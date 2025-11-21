c906c2921dbcf7e8a1166ef8a2ec6a1c93dccd64
Parsa Gholipour <12parsa@gmail.com>
2025-11-21 13:34:48 +0330

    fix: use generic type in useLiveQuery query promise

--- src/useLiveQuery.ts
+++ src/useLiveQuery.ts
@@ -1,6 +1,6 @@
 import {liveQuery} from "./liveQuery";
 import {useObservable} from "dexie-react-hooks";
 
-export default function useLiveQuery<TDatabase = any, T = any,TDefault = any>(queryCallback: (db: TDatabase) => Promise<any>, deps: any[] = [], initialValue?: TDefault) {
+export default function useLiveQuery<TDatabase = any, T = any, TDefault = any>(queryCallback: (db: TDatabase) => Promise<T> | T, deps: any[] = [], initialValue?: TDefault) {
   return useObservable<T, TDefault>(() => liveQuery(queryCallback), deps, initialValue as TDefault);
 }
